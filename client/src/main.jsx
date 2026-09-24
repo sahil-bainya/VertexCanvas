dafa62884bfd8067973853291687a668b2f76910
Sahil Bainya <sahilbainya2005@gmail.com>
2026-09-24 14:44:27 +0530

    zz

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,7 +10,7 @@ import { Toaster } from "react-hot-toast";
 import "./index.css";
 import App from "./App.jsx";
 import store from "./store/store.js";
-import { AuthLayout, Protected } from "./components";
+import { AuthLayout } from "./components";
 import {
   AuthPage,
   BoardPage,
@@ -24,31 +24,10 @@ const savedTheme = localStorage.getItem("theme") || "default";
 document.documentElement.setAttribute("data-theme", savedTheme);
 
 const router = createBrowserRouter([
-  // Public (redirect if logged in)
-  {
-    path: "/",
-    element: (
-      <Protected>
-        <LandingPage />
-      </Protected>
-    ),
-  },
-  {
-    path: "/login",
-    element: (
-      <Protected>
-        <AuthPage />
-      </Protected>
-    ),
-  },
-  {
-    path: "/register",
-    element: (
-      <Protected>
-        <AuthPage />
-      </Protected>
-    ),
-  },
+  // Public
+  { path: "/", element: <LandingPage /> },
+  { path: "/login", element: <AuthPage /> },
+  { path: "/register", element: <AuthPage /> },
 
   // Protected (App layout wrapper)
   {
--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.DEV
-  ? "http://localhost:3000"
-  : "https://vertexcanvas.onrender.com";
-// const API_URL = "http://localhost:3000";
+// const API_URL = import.meta.env.DEV
+//   ? "http://localhost:3000"
+//   : "https://vertexcanvas.onrender.com";
+const API_URL = "http://localhost:3000";
 const api = axios.create({
   baseURL: `${API_URL}/api`,
   withCredentials: true,
